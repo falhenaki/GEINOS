376ee95d3c884d35b1b3498d5161f86bac6a0988
Fawaz <faa5019@rit.edu>
2018-02-21 16:24:12 -0500

    added sample request to models

--- genios_app/genios_app/models.py
+++ genios_app/genios_app/models.py
@@ -1,1 +1,13 @@
 ###class definitions go here###
+# this is is just a simple call to see if we are able to communicate with the device/send data
+import requests
+
+filepath = 'body.txt'
+
+with open(filepath) as fh:
+  data = fh.read()
+
+headers = {'Accept':'application/xml', 'Content-Type' : 'application/xml'}
+r = requests.put('http://localhost:8181/restconf/config/network-topology:network-topology/topology/topology-netconf/node/new-netconf-device', headers = headers, data = data)
+
+print(r)
