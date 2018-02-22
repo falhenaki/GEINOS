6655a26517d5738c552a6b689c1b78cc2e6faaa5
Doug Gawne <dfg9279@g.rit.edu>
2018-02-22 02:19:03 -0800

    Basic hard coded login as well as framework setup steps

--- genios_app/genios_app/__init__.py
+++ genios_app/genios_app/__init__.py
@@ -1,4 +1,9 @@
 from flask import Flask
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
+app.config.from_object(__name__)
+
+app.config.update(dict(
+    SECRET_KEY='something_secret'
+))
 
 import genios_app.views
--- genios_app/genios_app/views.py
+++ genios_app/genios_app/views.py
@@ -1,9 +1,35 @@
-from flask import Flask
+from flask import Flask, render_template, session, request, g, redirect, url_for, abort, flash
 from genios_app import app
 
 @app.route('/')
-def hello_world():
-	return 'Hello, World!'
+def start_app():
+	"""
+	Initial route of the app renders a basic login template or connection test if the user is logged in
+	:return: login template or connection test page
+	"""
+	if not session.get('logged_in'):
+		return render_template('login.html')
+	else:
+		return render_template('connection_test.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+	"""
+	route to submit login requests to TODO add a database connection
+	:return: returns to previous route with a potentially changed login status
+	"""
+	error = None
+	if request.form['password'] == 'password' and request.form['username'] == 'admin':
+		session['logged_in'] = True
+	else:
+		flash('Incorrect Username and/or Password')
+	return start_app()
+
+@app.route('/logout', methods=['GET', 'POST'])
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return start_app()
 
 if __name__ == "__main__":
 	app.run()
