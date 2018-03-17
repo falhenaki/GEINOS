6ebb18149c2dda396d938d7b85fa1e5619933c92
Qasim <Qasim>
2018-03-16 20:35:01 -0400

    new structure

---
+++ geinos/app/__init__.py
@@ -0,0 +1,34 @@
+# Import flask and template operators
+from flask import Flask, render_template
+
+# Import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
+# Define the WSGI application object
+app = Flask(__name__)
+
+# Configurations
+app.config.from_object('config')
+
+# Define the database object which is imported
+# by modules and controllers
+db = SQLAlchemy(app)
+
+engine = create_engine('mysql+mysqlconnector://admin:password@bitforcedev.se.rit.edu/se_project')
+
+# Sample HTTP error handling
+@app.errorhandler(404)
+def not_found(error):
+    return render_template('404.html'), 404
+
+# Import a module / component using its blueprint handler variable (mod_auth)
+from app.baseline_module.views import mod_auth as auth_mod
+
+# Register blueprint(s)
+app.register_blueprint(auth_mod)
+# app.register_blueprint(xyz_module)
+# ..
+# Build the database:
+# This will create the database file using SQLAlchemy
+#db.create_all()
---
+++ geinos/app/baseline_module/user_routing.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, session, request, g, redirect, url_for, abort, flash
+from werkzeug.security import generate_password_hash, check_password_hash
+from app.baseline_module import __init__, models, auth_module, db_connector
+from sqlalchemy.orm import sessionmaker
+from app.baseline_module.models import *
+from app.baseline_module import genios_decorators
+from app import app
+
+#engine = create_engine('sqlite:///genios_db.db', echo=True)
+'''
+@app.route('/genios/users/login', methods=['POST'])
+def login():
+    return
+'''
+@app.route('/genios/add_user', methods=['POST'])
+@genios_decorators.requires_roles("ADMIN")
+def add_User():
+
+    POST_USERNAME = request.form['usr']
+    POST_PASSWORD = request.form['password']
+    POST_RETYPE_PASS = request.form['retypepassword']
+    POST_EMAIL = request.form['email']
+    POST_ROLE = str(request.form['role'])
+    if POST_PASSWORD != POST_RETYPE_PASS:
+
+        print('Passwords did not match')
+        return render_template('users.html')
+    else:
+        if auth_module.add_user(POST_USERNAME, POST_PASSWORD, POST_EMAIL):
+            auth_module.change_user_role(POST_USERNAME, POST_ROLE)
+            print("User added sucessfully")
+        else:
+            print("Username already taken")
+
+    return render_template("users.html")
