e8a04d3e4ea954108d9f5996049bf0934d0474f0
Brian <bxy3204@rit.edu>
2018-03-22 20:46:36 -0400

    Change UI to SPA

---
+++ geinos/app/src/DeviceGroups.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './DeviceGroups.css';
+class DeviceGroups extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <h2>Welcome to GEINOS</h2>
+            </div>
+        );
+    }
+}
+
+export default DeviceGroups;
---
+++ geinos/app/src/Devices.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './Devices.css';
+class Devices extends Component {
+    render() {
+        return (
+            <div className="Home">
+                <h2>Welcome to GEINOS</h2>
+            </div>
+        );
+    }
+}
+
+export default Devices;
