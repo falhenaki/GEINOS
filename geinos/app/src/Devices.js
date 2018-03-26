000b313196c6c65ec05b8b006472af7b86a640e2
Brian <bxy3204@rit.edu>
2018-03-26 13:36:47 -0400

    Many updates

--- geinos/app/src/Devices.js
+++ geinos/app/src/Devices.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 import './Devices.css';
+var stuff=[]
+var id=''
 class Devices extends Component {
+    constructor() {
+        super();
+        this.state = { items: [] };
+    }
+
+
+    componentDidMount() {
+        fetch(`https://jsonplaceholder.typicode.com/posts/1`)
+            .then(result=> result.json()).then((items) => {
+               // console.log(items);
+              //  console.log(items.length);
+                this.setState({items: items});
+                stuff=items;
+                console.log(stuff);
+            }
+            );
+    }
+
     render() {
+        const data = stuff;
         return (
-            <div className="Home">
-                <h2>Welcome to GEINOS</h2>
+            <div>
+                {data.map(function(d, idx){
+                    return (<li key={idx}>{d.title}</li>)
+                })}
             </div>
         );
     }
