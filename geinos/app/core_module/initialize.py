99e215214f9ec570eafad14110fd09e8a38dea6f
Qasim Ali <aliqasim9907@gmail.com>
2018-03-26 15:33:35 -0400

    startpage error fix

--- geinos/app/core_module/initialize.py
+++ geinos/app/core_module/initialize.py
@@ -5,6 +5,6 @@ from app.core_module import api_plugs
 def initialize_APIs():
     api = Api(app)
     api.add_resource(api_plugs.HelloWorld, '/abcde')
-    api.add_resource(api_plugs.Login, '/login')
+    api.add_resource(api_plugs.Login, '/login', '/')
     api.add_resource(api_plugs.Users, '/users')
     api.add_resource(api_plugs.Devices, '/devices')
