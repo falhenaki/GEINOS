cfd990c9b54932255ae3ae3282414f1838753e73
Qasim Ali <aliqasim9907@gmail.com>
2018-03-29 19:03:35 -0400

    integrated ajay's changes to talk to device

---
+++ geinos/app/core_module/device_connector.py
@@ -0,0 +1,93 @@
+import sys, os, warnings
+from app.pyorbit_module import Device, ConnectError
+from app.pyorbit_module.services import Config, Status
+
+config="""
+<config>
+    <system xmlns="urn:ietf:params:xml:ns:yang:ietf-system">
+        <ntp>
+            <use-ntp>true</use-ntp>
+            <ntp-server>
+                <address>1.1.1.1</address>
+            </ntp-server>
+        </ntp>
+    </system>
+</config>
+"""
+
+ntp_template="""
+<config>
+    <system xmlns="urn:ietf:params:xml:ns:yang:ietf-system">
+        <ntp>
+            <use-ntp>true</use-ntp>
+            <ntp-server>
+                <address>{{ ntp_server }}</address>
+            </ntp-server>
+        </ntp>
+    </system>
+</config>
+"""
+
+def set_config_with_template(host, user, passwd):
+    try:
+        dev = Device(host=host,username=user,password=passwd)
+        dev.open()
+        with Config(dev) as cm:
+            rsp = cm.load(template=ntp_template, template_vars={'ntp_server':'1.1.1.1'})
+            print(rsp)
+            rsp = cm.validate()
+            print(rsp)
+            rsp = cm.commit()
+            print(rsp)
+    except ConnectError as err:
+        print ("Cannot connect to device: {0}".format(err))
+        return
+
+def get_uptime(host, user, passwd):
+    try:
+        dev = Device(host=host,username=user,password=passwd)
+        dev.open()
+        with Status(dev) as st:
+            uptime="""/system/uptime/seconds"""
+
+            # JSON
+            out = st.get(filter=('xpath',uptime),format='json')
+            print(out)
+
+            # ODICT
+            #out = st.get(filter=('xpath',uptime),format='odict')
+            #print(out)
+
+            # XML
+            #out = st.get(filter=('xpath',uptime))
+            #print(out)
+
+    except ConnectError as err:
+        print ("Cannot connect to device: {0}".format(err))
+        return
+
+def set_config(host, user, passwd):
+    try:
+        dev = Device(host=host,username=user,password=passwd)
+        dev.open()
+        with Config(dev) as cm:
+            rsp = cm.load(content=config)
+            print(rsp)
+            rsp = cm.validate()
+            print(rsp)
+            rsp = cm.commit()
+            print(rsp)
+    except ConnectError as err:
+        print ("Cannot connect to device: {0}".format(err))
+        return
+
+def get_config(host, user, passwd):
+    try:
+        dev = Device(host=host,username=user,password=passwd)
+        dev.open()
+        with Config(dev) as cm:
+            out = cm.get(format='json')
+            print(out)
+    except ConnectError as err:
+        print ("Cannot connect to device: {0}".format(err))
+        return
