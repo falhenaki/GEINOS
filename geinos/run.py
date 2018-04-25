f238c286ad76e0c80bf636035433f0926c92599f
Qasim Ali <aliqasim9907@gmail.com>
2018-04-25 15:34:40 -0400

    creating template folders

--- geinos/run.py
+++ geinos/run.py
@@ -1,9 +1,16 @@
 # Run a test server.
 from app import app
 from app.core.api import initialize
+import os
 #import subprocess
 
 #subprocess.call(['app/pyorbit/tst_scr.sh'])
+
+if not os.path.exists(app.config['UPLOADS_FOLDER']):
+    os.makedirs(app.config['UPLOADS_FOLDER'])
+
+if not os.path.exists(app.config['APPLIED_PARAMS_FOLDER']):
+    os.makedirs(app.config['APPLIED_PARAMS_FOLDER'])
 
 initialize.initialize_APIs()
 
