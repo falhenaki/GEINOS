6f134df12b4d87543230da610da59506f2f512d0
Brian <bxy3204@rit.edu>
2018-05-31 16:58:27 -0400

    Return challenge password for SCEP

--- geinos/app/core/SCEP/SCEP_server.py
+++ geinos/app/core/SCEP/SCEP_server.py
@@ -1,7 +1,20 @@
 import requests
-
-
 from requests_ntlm import HttpNtlmAuth
 
-result=requests.get("http://192.168.56.102/certsrv/mscep_admin/",auth=HttpNtlmAuth('domain\\administrator','Password12'))
-print(result.content.decode('UTF-16'))
+"""
+Gets the webpage of a SCEP server with a challenge password
+TODO: User defined name and password
+"""
+
+def get_challenge_password():
+    result = requests.get("http://192.168.56.102/certsrv/mscep_admin/",
+                          auth=HttpNtlmAuth('domain\\administrator', 'Password12'))
+    page = result.content.decode('UTF-16')
+    str1 = 'The enrollment challenge password is: <B'
+    i = 0
+    for index,s in enumerate(page.split('>')):
+        if s.find(str1) is 1:
+            i = index + 1
+    password = page.split('>')[i]
+    print(page.split('>')[i][:17])
+    return password
