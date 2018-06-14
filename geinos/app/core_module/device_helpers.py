682c4d6527b7c09570eaf8139db058603faf1bf2
Brian <bxy3204@rit.edu>
2018-06-14 10:50:11 -0400

    update

--- geinos/app/core_module/device_helpers.py
+++
@@ -1,7 +0,0 @@
-from app.core_module import db_connector
-
-def add_list_of_devices(entries):
-    for entry in entries:
-        #entry.split(",")
-        db_connector.add_device(int(entry[0]), int (entry[1]), int (entry[2]))
-    return True
