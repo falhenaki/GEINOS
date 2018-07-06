85ff12bba0dbe0147ea1281101d2dab7002a2c09
Qasim Ali <aliqasim9907@gmail.com>
2018-07-06 02:49:45 -0400

    more test data

---
+++ data/device_groups.sql
@@ -0,0 +1,8 @@
+'g1', '2018-07-06 02:24:58', NULL, '{\'model_number\': \'MDS Orbit MCR-1E2S\', \' location\': \'Florida\'}'
+'g2', '2018-03-09 01:21:30', NULL, '{\'model_number\': \'MDS Orbit MCR-4E2S\', \' location\': \'California\'}'
+'g3', '2018-02-08 04:21:22', NULL, '{\'model_number\': \'MDS Orbit MCR-2E1S\', \' location\': \'New York\'}'
+'g4', '2018-01-07 06:22:56', NULL, '{\'model_number\': \'MDS Orbit ECR\', \' location\': \'New Jersey\'}'
+'g1', '2018-07-06 02:24:58', NULL, '{\'model_number\': \'MDS Orbit ECR\'}'
+'g2', '2018-03-09 01:21:30', NULL, '{\'model_number\': \'MDS Orbit MCR-4E2S\'}'
+'g3', '2018-02-08 04:21:22', NULL, '{\'model_number\': \'MDS Orbit MCR-2E1S\'}'
+'g4', '2018-01-07 06:22:56', NULL, '{\'model_number\': \'MDS Orbit MCR-1E2S\'}'
